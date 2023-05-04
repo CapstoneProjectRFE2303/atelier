eff4857efee07aac90cf26a1944b7552e6ff9e21
lederius <lederius.franklin@gmail.com>
2023-05-04 14:23:55 -0700

    needs to be deleted

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import StyleSelector from './StyleSelector.jsx';
 
 const App = () => {
   return (
     <div>
-      <p>Hello, World!</p>
+      <StyleSelector />
+
     </div>
+
   );
 };
 
---
+++ client/src/components/StyleSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StyleSelector = () => {
+
+  return (
+    <div>
+      <h2>styleSelector</h2>
+    </div>
+  )
+}
+export default StyleSelector;
