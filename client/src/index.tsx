22e279f430ec509805ea1b49a04d9989d5799870
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-05-15 21:16:48 -0400

    feat: add daisyui to project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app/App';
-import './styles/main.scss';
+import './styles/index.css';
 
 const container = document.getElementById('root');
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: [
+    './client/src/**/*.{html,js,jsx,ts,tsx}'
+  ],
+  plugins: [
+    require('tailwindcss'),
+    require('daisyui')
+  ],
+};
